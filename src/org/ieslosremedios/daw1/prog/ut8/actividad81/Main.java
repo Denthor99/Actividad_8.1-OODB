abd9e60d359d5648d0bb718d8dd128f3e234b1ea
Den <drodsan2708@g.educaand.es>
2023-05-05 14:53:34 +0200

    drodsan2708 - ejercicio iniciado en clase - 05/05/2023

---
+++ src/org/ieslosremedios/daw1/prog/ut8/actividad81/Main.java
@@ -0,0 +1,7 @@
+package org.ieslosremedios.daw1.prog.ut8.actividad81;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/org/ieslosremedios/daw1/prog/ut8/actividad81/Persona.java
@@ -0,0 +1,9 @@
+package org.ieslosremedios.daw1.prog.ut8.actividad81;
+
+import javax.persistence.Transient;
+
+public class Persona {
+    private String nombre;
+    private Integer edad;
+    private String dni;
+}
