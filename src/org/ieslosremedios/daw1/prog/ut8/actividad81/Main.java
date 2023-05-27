98b449ecdf6e5a07ec5582430b39c7405d918f38
Den <drodsan2708@g.educaand.es>
2023-05-27 22:58:59 +0200

    drodsan2708 - Finalización de la actividad

--- src/org/ieslosremedios/daw1/prog/ut8/actividad81/Main.java
+++
@@ -1,7 +0,0 @@
-package org.ieslosremedios.daw1.prog.ut8.actividad81;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/org/ieslosremedios/daw1/prog/ut8/actividad81/Ordenadores.java
+++ src/org/ieslosremedios/daw1/prog/ut8/actividad81/Ordenadores.java
@@ -5,12 +5,11 @@ import java.io.Serializable;
 import java.math.BigInteger;
 import java.sql.Timestamp;
 
-@Entity (name = "Ordenador")
-@Embeddable
+@Entity
 public class Ordenadores implements Serializable {
     @Id
-    @SequenceGenerator(name = "idPcs", initialValue = 10, allocationSize = 10)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "idPcs")
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "IdsPc10en10")
+    @SequenceGenerator(initialValue = 10,allocationSize = 10, name = "IdsPc10en10")
     private Integer id;
     private String ip;
     private String numSerie;
@@ -18,16 +17,86 @@ public class Ordenadores implements Serializable {
     private BigInteger discoDuro;
     private String cpu;
     private Timestamp fechaCompra;
-
+    private Boolean SOActivado;
     public Ordenadores() {
     }
 
-    public Ordenadores(String ip, String numSerie, BigInteger memoria, BigInteger discoDuro, String cpu, Timestamp fechaCompra) {
+    public Ordenadores(String ip, String numSerie, BigInteger memoria, BigInteger discoDuro, String cpu, Timestamp fechaCompra,Boolean SOActivado) {
         this.ip = ip;
         this.numSerie = numSerie;
         this.memoria = memoria;
         this.discoDuro = discoDuro;
         this.cpu = cpu;
         this.fechaCompra = fechaCompra;
+        this.SOActivado=SOActivado;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public BigInteger getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(BigInteger memoria) {
+        this.memoria = memoria;
+    }
+
+    public BigInteger getDiscoDuro() {
+        return discoDuro;
+    }
+
+    public void setDiscoDuro(BigInteger discoDuro) {
+        this.discoDuro = discoDuro;
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public Timestamp getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Timestamp fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public Boolean getSOActivado() {
+        return SOActivado;
+    }
+
+    public void setSOActivado(Boolean SOActivado) {
+        this.SOActivado = SOActivado;
+    }
+
+    @Override
+    public String toString() {
+        return "Ordenadores{" +
+                "ip='" + ip + '\'' +
+                ", numSerie='" + numSerie + '\'' +
+                ", memoria=" + memoria +
+                ", discoDuro=" + discoDuro +
+                ", cpu='" + cpu + '\'' +
+                ", fechaCompra=" + fechaCompra +
+                ", SOActivado=" + SOActivado +
+                '}';
     }
 }
